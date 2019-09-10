ad1e6f2aa59478a5a888c73f3462ffea9dfb5628
ratheemohan <mohan.kankalapati@gmail.com>
2019-09-10 20:42:02 +0100

    fix logging

--- src/main/kotlin/com/revolut/test/transfersvc/util/LoggingUtils.kt
+++ src/main/kotlin/com/revolut/test/transfersvc/util/LoggingUtils.kt
@@ -1,11 +1,11 @@
 package com.revolut.test.transfersvc.util
 
-import org.apache.log4j.LogManager
-import org.apache.log4j.Logger
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-inline fun<reified T> T.logger(): Logger {
-    if(T::class.isCompanion){
-        return LogManager.getLogger(T::class.java.enclosingClass)
+inline fun <reified T> T.logger(): Logger {
+    if (T::class.isCompanion) {
+        return LoggerFactory.getLogger(T::class.java.enclosingClass)
     }
-    return LogManager.getLogger(T::class.java)
+    return LoggerFactory.getLogger(T::class.java)
 }
